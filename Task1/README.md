2aacddaa10eacf11a20c6b17c2196a33502404f1
Дмитрий <56546102+Dmitry-DS@users.noreply.github.com>
2020-03-02 20:05:22 +0200

    add all tasks

---
+++ Task1/README.md
@@ -0,0 +1,19 @@
+# Rozetka 1 задание
+
+
+1. Склонировать репозиторий
+
+        git clone git@github.com:Dmitry-DS/Rozetka.git
+
+2. Перейти в папку Task1
+3. Запустить установку библиотек
+
+        composer update
+
+4. Запуск тестов с отчетом
+
+        vendor\bin\codecept run --html 
+        
+5. Отчет генерируется в папке:
+
+        C:\project\Rozetka\Task1\tests\_output\report.html
---
+++ Task1/tests/acceptance/FirstCest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Class FirstCest
+ *
+ * @group firstTest
+ */
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function firstTest(AcceptanceTester $I)
+    {
+        $expected ="Доставка по всей Украине";
+
+        $I->amOnPage('/');
+        $actual = $I->grabAttributeFrom('meta[name="description"]', 'content');
+        $I->assertStringContainsString($expected, $actual, "Тест упал потому что не нашел текст доставка по Украине");
+    }
+
+    // tests
+    public function secondTest(AcceptanceTester $I)
+    {
+        $expected ="Доставка по всей Украине";
+        $I->amOnPage('/');
+        $I->see($expected);
+    }
+}
